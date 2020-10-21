88b1fdc31768f48b4554c23a7ec4e51cc0ef1940
unknown <tanmay.sushant@iitg.ac.in>
2020-10-22 02:36:09 +0530

    Added room creation code FINALLYgit status!

---
+++ techno-static/src/components/Join/Join.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Join.css';
+
+const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    return (
+        <div className= "joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="UserName" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}/></div>
+                <div><input placeholder="RoomCode" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}/></div>
+                {/* <Link onClick={event => (!name||!room) ? event.preventDefault():null} to={'/game?:name='+name+'&:room='+room}>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link> */}
+                <Link 
+                    onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+                    to={{
+                        pathname: "/game",
+                        state: {
+                            name: name,
+                            roomid: room,
+                        },
+                    }}>
+                        <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
+        </div>   
+    )
+}
+
+export default Join;
