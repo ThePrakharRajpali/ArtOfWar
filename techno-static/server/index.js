548b0afa08a231eeb5854764b960b61cb4c71738
piyush-tiwari <t.piyush@iitg.ac.in>
2020-10-23 20:13:09 +0530

    changes in Room's state.

--- techno-static/server/index.js
+++ techno-static/server/index.js
@@ -13,13 +13,17 @@ const io = socketio(server);
 const maxTime = 20*60;
 
 const initState = {
-    squares: [],
-    pieces: [],
+    squares: makearray(),
+    pieces: makepieces(),
     blueScore: 0,
     redScore: 0,
     isGameOn: true,
     blueTurn: false,
     isSetup: true,
+    numBlue: 0,
+    numRed: 0,
+    initialRedPiece: [6, 1, 1, 7, 5, 5, 4, 4, 3, 2, 1, 1],
+    initialBluePiece: [6, 1, 1, 7, 5, 5, 4, 4, 3, 2, 1, 1],
 };
 
 const room={
@@ -55,23 +59,21 @@ io.on('connection', (socket) => {
                 rooms[data].red = socket.id;
                 rooms[data].limit = 1;
                 rooms[data].roomState = initState;
-                rooms[data].blueReady = false;
-                rooms[data].redReady = false;
                 timeintervals[data]={}
                 timeintervals[data].red=maxTime;
                 timeintervals[data].blue=maxTime;
-                socket.emit("roomid", { roomid: data, isPlayerBlue: false});
+                socket.emit("roomid", { roomid: data, isPlayerBlue: false, roomState: rooms[data].roomState});
                 console.log("room created?");
             } else {
                 if (rooms[data].red === undefined) {
                     rooms[data].red = socket.id;
                     rooms[data].limit += 1;
-                    socket.emit("roomid", { roomid: data, isPlayerBlue: false});
+                    socket.emit("roomid", { roomid: data, isPlayerBlue: false, roomState: rooms[data].roomState});
                     console.log("red boi in");
                 } else {
                     rooms[data].blue = socket.id;
                     rooms[data].limit += 1;
-                    socket.emit("roomid", { roomid: data,isPlayerBlue: true});
+                    socket.emit("roomid", { roomid: data,isPlayerBlue: true, roomState: rooms[data].roomState});
                     console.log("blue boi in");
                 }
             }
@@ -99,16 +101,21 @@ io.on('connection', (socket) => {
     socket.on("newPieceAdd", (data) =>{
         rooms[socketIds[socket.id]].roomState.squares = data.squares;
         rooms[socketIds[socket.id]].roomState.pieces = data.pieces;
+        rooms[socketIds[socket.id]].roomState.initialBluePiece = data.initialBluePiece;
+        rooms[socketIds[socket.id]].roomState.initialRedPiece = data.initialRedPiece;
+        rooms[socketIds[socket.id]].roomState.numRed = data.numRed;
+        rooms[socketIds[socket.id]].roomState.numBlue = data.numBlue;
 
         io.to(socketIds[socket.id]).emit("newPieceAddedInfo", rooms[socketIds[socket.id]].roomState);
     });
 
     socket.on("win", (data) => {
         io.to(socketIds[socket.id]).emit("Ended", data);
+        rooms[socketIds[socket.id]].roomState.isGameOn = false;
         clearInterval(intervals[socketIds[socket.id]]);
 
-        if(data===0 && rooms[socketIds[socket.id]].redScore < 180)  rooms[socketIds[socket.id]].redScore += 180;
-        if(data===1 && rooms[socketIds[socket.id]].blueScore < 180)  rooms[socketIds[socket.id]].blueScore += 180;
+        if(data===0 && rooms[socketIds[socket.id]].roomsState.redScore < 180)  rooms[socketIds[socket.id]].roomState.redScore += 180;
+        if(data===1 && rooms[socketIds[socket.id]].roomsState.blueScore < 180)  rooms[socketIds[socket.id]].roomState.blueScore += 180;
 
     });
 
@@ -128,6 +135,7 @@ io.on('connection', (socket) => {
                     --timeintervals[socketIds[socket.id]].blue;
                     if (timeintervals[socketIds[socket.id]].blue < 1) {
                         io.to(socketIds[socket.id]).emit("Ended", 0);
+                        rooms[socketIds[socket.id]].roomState.isGameOn = false;
                         rooms[socketIds[socket.id]].roomState.redScore += 180;
                         clearInterval(intervals[socketIds[socket.id]]);
                     }
@@ -135,6 +143,7 @@ io.on('connection', (socket) => {
                     --timeintervals[socketIds[socket.id]].red;
                     if (timeintervals[socketIds[socket.id]].red < 1) {
                         io.to(socketIds[socket.id]).emit("Ended", 1);
+                        rooms[socketIds[socket.id]].roomState.isGameOn = false;
                         rooms[socketIds[socket.id]].roomState.blueScore += 180;
                         clearInterval(intervals[socketIds[socket.id]]);
                     }
@@ -151,6 +160,104 @@ io.on('connection', (socket) => {
 app.use(router);
 
 server.listen(PORT,() => console.log('Server has started on port',PORT));
+
+function makearray() {
+    let board = [];
+
+    for(var i = 0; i<12; i++) {
+
+        for(var j =0; j<10;j++) {
+            let square = {
+                isLake: isLake(i,j),
+                pieceid: {
+                    isBlue: null, //set this as 0 or 1, not true or false.
+                    index: null,
+                },
+                isHighlighted: false,
+                isPurple: false,
+                hasPiece: false,
+            };
+
+            board.push(square);
+        }
+    }
+    return board;
+}
+
+function makepieces() { //pieces[0][index] are red, pieces[1][index] are blue.
+    let pieces = [];
+
+    let redPieces = makePieceArray(false);
+    let bluePieces = makePieceArray(true);
+    
+    pieces.push(redPieces);
+    pieces.push(bluePieces);
+
+    return pieces;
+}
+
+function makePieceArray(isBlue) {
+    let part = [];
+
+    for(let i = 0;i<6;i++)
+        part.push(pieceMaker(isBlue,-1,false));
+
+    part.push(pieceMaker(isBlue,0,false));
+    part.push(pieceMaker(isBlue,1,true));
+
+    for(let i=0;i<7;i++)
+        part.push(pieceMaker(isBlue,2,true));
+
+    for(let i=0;i<5;i++)
+        part.push(pieceMaker(isBlue,3,true));
+
+    for(let i=0;i<5;i++)
+        part.push(pieceMaker(isBlue,4,true));
+
+    for(let i=0;i<4;i++)
+        part.push(pieceMaker(isBlue,5,true));
+
+    for(let i=0;i<4;i++)
+        part.push(pieceMaker(isBlue,6,true));
+
+    for(let i=0;i<3;i++)
+        part.push(pieceMaker(isBlue,7,true));
+
+    for(let i=0;i<2;i++)
+        part.push(pieceMaker(isBlue,8,true));
+
+    part.push(pieceMaker(isBlue,9,true));
+    part.push(pieceMaker(isBlue,10,true));
+
+    return part;
+}
+
+function isLake(i,j) {
+    if(i>=4 && i<=5 && j>=2 && j<=3)
+        return true;
+    if(i===6 && j===3)
+        return true;
+    if(i===5 && j===6)
+        return true;
+    if(i>=6 && i<=7 && j>=6 && j<=7)
+        return true;
+
+    return false;
+}
+
+function pieceMaker(isBlue, rank, isMovable) {
+    let piece = {
+        isBlue: isBlue,
+        pos: null,
+        rank: rank,
+        isMovable: isMovable,
+        isAlive: true,
+    };
+
+    return piece;
+}
+
+
 
 //TODO
 //Restrict opposite movement when not in turn (done)
